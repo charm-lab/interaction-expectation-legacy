d3f4a252b240409175eb9f1ab9f4924878830bbf
msalvato <msalvato@stanford.edu>
2022-01-25 10:25:14 -0800

    removed junk files

--- experiments1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python run.py --directories_file configs/machine_directories/juno_directories.yml --config_file configs/experiments/experiment_config_f1.yml --experiment_name fold1_vs6 --test_ranges [6] --times_ahead [6] --single_times True --data_type vertices
-python run.py --directories_file configs/machine_directories/juno_directories.yml --config_file configs/experiments/experiment_config_f1.yml --experiment_name fold1_vs12 --test_ranges [12] --times_ahead [12] --single_times True --data_type vertices
-python run.py --directories_file configs/machine_directories/juno_directories.yml --config_file configs/experiments/experiment_config_f1.yml --experiment_name fold1_vs24 --test_ranges [24] --times_ahead [24] --single_times True --data_type vertices
-
-python run.py --directories_file configs/machine_directories/juno_directories.yml --config_file configs/experiments/experiment_config_obj.yml --experiment_name obj_v_0_0 --test_ranges [12] --times_ahead [12] --single_times True --data_type vertices
-5
--- experiments4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python run.py --directories_file configs/machine_directories/juno_directories.yml --config_file configs/experiments/experiment_config_f4.yml --experiment_name fold4_vs6 --test_ranges [6] --times_ahead [6] --single_times True --data_type vertices
-python run.py --directories_file configs/machine_directories/juno_directories.yml --config_file configs/experiments/experiment_config_f4.yml --experiment_name fold4_vs12 --test_ranges [12] --times_ahead [12] --single_times True --data_type vertices
-python run.py --directories_file configs/machine_directories/juno_directories.yml --config_file configs/experiments/experiment_config_f4.yml --experiment_name fold4_vs24 --test_ranges [24] --times_ahead [24] --single_times True --data_type vertices
-
-python run.py --directories_file configs/machine_directories/juno_directories.yml --config_file configs/experiments/experiment_config_obj.yml --experiment_name obj_v_100_0 --test_ranges [12] --times_ahead [12] --single_times True --joint_noise_level 100 --transl_noise_level 0 --data_type vertices
